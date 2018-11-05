1db6aa9b1976017b894cb7ccc673baa271e08d21
BestCodeMaker <1289707016@qq.com>
2018-11-05 19:55:07 +0800

    添加git02到本地工仓库

--- git_test/src/cn/itcast/test/Git01.java
+++ git_test/src/cn/itcast/test/Git01.java
@@ -3,5 +3,7 @@ package cn.itcast.test;
 public class Git01 {
     public static void main(String[] args) {
         System.out.println("我是用来测试git的！");
+
+        System.out.println("我现在使用分支devp来测试的！");
     }
 }
