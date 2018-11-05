5567ae8c7e9ec3bcbfc2144f090bb2685c71c1aa
BestCodeMaker <1289707016@qq.com>
2018-11-05 20:27:29 +0800

    添加git01到本地工仓 转变为原始分支

--- git_test/src/cn/itcast/test/Git01.java
+++ git_test/src/cn/itcast/test/Git01.java
@@ -2,8 +2,7 @@ package cn.itcast.test;
 
 public class Git01 {
     public static void main(String[] args) {
-        System.out.println("我是用来测试git的！");
 
-        System.out.println("我现在使用分支devp来测试的！");
+        System.out.println("我又变成了最原始的分支");
     }
 }
